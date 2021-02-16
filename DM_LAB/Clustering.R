aeed5d082839b41621f2f3980c95ea69d22df469
VaibhavRast <59047444+VaibhavRast@users.noreply.github.com>
2021-02-16 19:37:17 +0530

    Update Clustering.R

--- DM_LAB/Clustering.R
+++ DM_LAB/Clustering.R
@@ -1,4 +1,4 @@
-
+#Kmeans
 library(factoextra)
 
 data <- iris
@@ -10,16 +10,19 @@ fviz_cluster(kModel, data, geom="point")
 print(kModel.res)
 head(data)
 
+
+#Selecting 20 data points for k means clustering
+set.seed(123)
+index=sample(seq_len(nrow(data)),size = 20)
+df=data[index,] #Dataset with 20 points
+
+distance2 <- scale(dist(df,method="euclidian"))
+kModel2 <- kmeans(distance2,3)
+print(kModel2)
+fviz_cluster(kModel2,df,check.names=TRUE)
+
+# Hierarchial Clustering(Not Required)
 distance2 <- dist(data[,3:4],method="euclidian")
 hModel <- hclust(distance2,method="average")
 plot(hModel)
 
-data2<-mtcars
-distance3 <- dist(data2,method="euclidian")
-hModel2 <- hclust(distance3,method="average")
-plot(hModel2)
-
-abline(h=110,col="green")
-fit<-cutree(hModel2,k=3)
-table(fit)
-rect.hclust(hModel2,k=3,border = "red")
