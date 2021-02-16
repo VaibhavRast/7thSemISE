b48b2bff187f39760f1ae48c25e9af96d7dcf8d5
VaibhavRast <59047444+VaibhavRast@users.noreply.github.com>
2021-02-16 21:59:05 +0530

    Update Apriori.R

--- DM_LAB/Apriori.R
+++ DM_LAB/Apriori.R
@@ -7,12 +7,14 @@ itemSets<-apriori(Groceries,parameter = list(supp=0.01,conf=0.2,target="frequent
 inspect(sort(itemSets,by="support"))
 
 rules<-apriori(Groceries,parameter = list(supp=0.01,conf=0.2))
-inspect(rules[1:10])
+rules2<-(sort(rules,by="confidence"))
+inspect(rules2[1:10])
 
-library(RColorBrewer)
-arules::itemFrequencyPlot(Groceries, topN = 20,  
-                          col = brewer.pal(8, 'Pastel2'), 
+
+itemFrequencyPlot(Groceries, topN = 20,  
+                          col = "red", 
                           main = 'Relative Item Frequency Plot', 
                           type = "relative", 
                           ylab = "Item Frequency (Relative)")
 plot(rules,method = "graph",max=3)
+
